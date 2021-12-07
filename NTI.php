563fc108773849c6d048db0a6791152aa7b84ac1
Abdo381 <71750074+Abdo381@users.noreply.github.com>
2021-12-07 03:44:23 +0200

    Add files via upload

--- NTI.php
+++ NTI.php
@@ -1,14 +1,43 @@
-<?php
-$num1=0;
-if ($num1<= 50)
-        {
-        $num2=3.50;
+<html> 
+     <head> 
+  </head>
+  <body>
+  <table width="800px" height="600px" cellspacing="0px" cellpadding="0px">
+      <?php
+      /*
+      for($row=1;$row<=10;$row++)
+	  {
+         echo "<tr>";
+          for($col=1;$col<=8;$col++)
+		  {
+          $total=$row+$col;
+          if($total%2==0)
+		  {
+          echo "<td height=30px width=30px bgcolor=#FFFFFF></td>";
+          }
+		  else
+		  {
+          echo "<td height=30px width=30px bgcolor=#000000></td>";
+          }
+          }
+       echo "</tr>";
+    }*/
 
-}elseif($num1<=100)
-{
-    $num2=4.00;
-}else {
-    $num2=6.50;
-}
-echo $num2;
-?>
+    function Message($str,$str2) {
+      
+       if ($str=="a"&& $str2=="z") {
+         echo( $str="b");
+         echo( $str2="a");
+          
+       }
+      
+       
+   }
+
+   Message("a","z");
+
+          ?>
+
+  </table>
+  </body>
+  </html>
